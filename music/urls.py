8a495ced103f898a2fefe108cb3fa3bd25728f58
Aman208 <amankumar20804@gmail.com>
2020-04-10 09:57:07 +0530

    navbar added

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url 
 from .  import views
 
+app_name ='music'
+
 urlpatterns = [
-   url(r'' , views.index , name="index")
+   url(r'^$' , views.index , name="index"),
+   url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album , Song
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1> welcome to app </h1>")
+    album_list = Album.objects.all()
+    return render(request , 'music/index.html' , { 'all_album' : album_list })
+
+def detail(request , album_id):
+    return HttpResponse("<h1> welcome to Detail Page  </h1>")
